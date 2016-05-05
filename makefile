f86c7e1db26b843554b5cb4a38cb411ecfa3c73c
Danilo Santos <dan-desenvolvimento@hotmail.com>
2016-05-04 23:56:24 -0300

    Starting

---
+++ makefile
@@ -0,0 +1,8 @@
+name = my_disk.cpp
+mode = -std=c99
+#mode = -ansi
+
+all:
+
+remove:
+	rm *.o
---
+++ modules/hard_drive.h
@@ -0,0 +1,21 @@
+/******************************/
+/*Especificações HD           */
+/******************************/
+#define TRILHA_CILINDRO 5
+#define SETOR_TRILHA 60
+#define TRILHA_SUPERFICIE 10
+#define TAM_SETOR 512
+#define TAM_CLUSTER 4
+/******************************/
+
+typedef struct block{
+  unsigned char bytes_s[TAM_SETOR];
+}block;
+
+typedef struct sector_array{
+  block sector[SETOR_TRILHA];
+}sector_array;
+
+typedef struct track_array{
+  sector_array track[TRILHA_CILINDRO];
+} track_array;
